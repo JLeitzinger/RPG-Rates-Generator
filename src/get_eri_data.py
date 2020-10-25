04aa33ebcef6ac48836e85ed3c8841c9cdd3eb67
JLeitzinger <elaboratejohnny@gmail.com>
2020-10-25 14:21:35 -0400

    Initial Commit

---
+++ src/get_eri_data.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 13:07:45 2020
+
+@author: elabo
+"""
+
+from selenium import webdriver
+import pandas as pd
+import time
+
+url = 'https://online.erieri.com/Account/Login'
+ar_url = 'https://online.erieri.com/SA/AdvancedReports/'
+
+
+def eri_login(url):
+    f=pd.read_csv('auth/upass.txt', sep=',', header=None, index_col=None)
+    usr = f.iloc[0][0].strip()
+    pword = f.iloc[0][1].strip()
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+    email_box = f_xpath(driver, '//*[@id="UserName"]')
+    email_box.send_keys(usr)
+    pass_box = f_xpath(driver, '//*[@id="Password"]')
+    pass_box.send_keys(pword)
+    
+    f_xpath(driver, '//*[@id="LoginButton"]').click()
+    return(driver)
+
+
+def f_xpath(driver, xpath):
+    loc = driver.find_element_by_xpath(xpath)
+    return(loc)
+
+def get_advanced_report(url, ar_url):
+
+    driver = eri_login(url)
+    driver.get(ar_url)
+    time.sleep(3)
+    f_xpath(driver, '//*[@id="Toolbar #btnExcelExport"]/i').click()
+    time.sleep(3)
+    f_xpath(driver, '//*[@id="btnOK_AdvancedReportsToExcelModal1"]').click()
+
+
+get_advanced_report(url, ar_url)
